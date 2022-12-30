d2252171720b4a761365316e32f702147ee68c82
Matute <79936247+Matiasbenites@users.noreply.github.com>
2022-12-30 19:03:44 -0300

    User's route added

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,29 @@ const express = require("express");
 const userRoutes = express.Router();
 
 
+// Default route for find all users
 userRoutes.get("/", (req, res) => {
+  // const data = 
+
   res.json({ status: 200, data: {} });
 });
 
+
+// Default route for find a specific user
+// userRoutes.get("/:id", (req, res) => {
+//   const data = 
+
+//   res.json({ status: 200, data: {} });
+// });
+
+
+// Default route for create a new user
+// userRoutes.post("/create", (req, res) => {
+//   const data = 
+
+//   res.json({ status: 200, data: {} });
+// });
+
+
+// Default route for update a specific user
 module.exports = userRoutes;
