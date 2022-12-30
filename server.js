4559687696b4ec7647df4309ecd44a753bd9705b
Matute <79936247+Matiasbenites@users.noreply.github.com>
2022-12-30 20:44:52 -0300

    App finished and running correctly

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/users");
+const taskRoutes = require("./routes/tasks");
+const rolRoutes = require("./routes/roles");
 const logging = require("./middlewares/log");
 const authentication = require("./middlewares/authentication");
 const app = express();
@@ -12,19 +14,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api", authentication, userRoutes);
+app.use("/user", authentication, userRoutes);
+app.use("/task", authentication, taskRoutes);
 
-mongoose.connect =
-  ("mongodb://127.0.0.1:27017/no-relational-api",
-  (err) => {
-    if (err) {
-      console.log(
-        "Se produjo un error al intentar conectarse a MongoDB 💥💥💥"
-      );
-    } else {
-      console.log("Conectado a MongoDB 🍃🍃🍃 ");
-    }
-  });
+mongoose.connect("mongodb://127.0.0.1:27017/no-relational-api", (err) => {
+  if (err) {
+    console.log("Se produjo un error al intentar conectarse a MongoDB 💥💥💥");
+  } else {
+    console.log("Conectado a MongoDB 🍃🍃🍃 ");
+  }
+});
 
 app.listen(3000, "localhost", () => {
   console.log("Listening on localhost:3000 💫💫💫");
